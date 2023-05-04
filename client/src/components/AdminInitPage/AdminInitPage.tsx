360f4d24f86a7b01df9e967ac4ae4365358ece3e
Jônatas Gomes <jongaglima@gmail.com>
2023-05-04 12:09:22 -0300

    feat: Create header to all homepages and begin work on admin homepage

---
+++ client/src/components/AdminInitPage/AdminInitPage.tsx
@@ -0,0 +1,10 @@
+import InitHeader from "../InitHeader/InitHeader";
+import "../../index.css";
+
+export default function AdminInitPage() {
+  return (
+    <div className="patternContainer">
+      <InitHeader />
+    </div>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import AdminInitPage from "./components/AdminInitPage/AdminInitPage.tsx";
+import "./index.css";
+import LoginPage from "./components/LoginPage/LoginPage.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  
-    <App />
-  
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <AdminInitPage />
+);
